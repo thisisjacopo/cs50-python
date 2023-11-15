85576be84ea4e84a159497cd19e070e02fd43822
thisisjacopo <jacopo.rodighiero@icloud.com>
2023-11-15 15:06:46 +0100

    finish grocery prob

--- ProblemSets/ProblemSet3/grocery/grocery.py
+++ ProblemSets/ProblemSet3/grocery/grocery.py
@@ -1,17 +1,14 @@
 total = 0
 items = {}
-prompt = "Enter your order: "
+prompt = ""
 while True:
     try:
         item = input(prompt)
-        if item.title() in items:
-            items[item.title()] += 1
+        if item in items:
+            items[item] += 1
         else:
-            items[item.title()] = 1
+            items[item] = 1
     except (EOFError, KeyboardInterrupt):
-        print()
+        for item in sorted(items):
+            print(items[item], item.upper(), end='\n')
         break
-
-for item in sorted(items):
-    print(items[item], item.upper())
-
